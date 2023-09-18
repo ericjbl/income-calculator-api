e4936eb52266c78407d0294cff51e9f1514dfc6e
Eric Barbosa <eric.barbosa@zetaminusone.com>
2023-09-18 11:18:08 -0400

    Added admin endponit to create users.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,16 +5,22 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
-@UseGuards(AuthGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @Post('/admin')
+  createAdmin(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.userService.findAll();
   }
@@ -25,16 +31,19 @@ export class UserController {
   // }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
   @Put('/deactivate/:id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
   }
 
   @Put('/forgotpassword/:id')
+  @UseGuards(AuthGuard)
   forgotPassword(@Param('id') id: number, @Body() password: string) {
     return this.userService.forgotPassword(id,password)
   }
